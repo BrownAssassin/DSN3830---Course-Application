b00091658feff0883679a0b548c0963db3dc3010
Mrinank S <mrinank.sivakumar@ontariotechu.net>
2022-03-17 15:10:27 -0400

    Base Unity Projects Added

---
+++ Client/Assets/Scripts/Client.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+// Non-Unity Libraries
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+    public Transform myCube;
+    private static byte[] outBuffer = new byte[12];
+    private static IPEndPoint remoteEP;
+    private static Socket clientSoc;
+    private float[] position;
+    private float[] prevPosition;
+    private float elapsedTime;
+
+    public static void RunClient()
+    {
+        try
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            remoteEP = new IPEndPoint(ip, 11111);
+
+            clientSoc = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Exception: " + e.ToString());
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCube = GameObject.Find("Cube").transform;
+
+        RunClient();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            outBuffer = Encoding.UTF8.GetBytes("shutdown");
+            clientSoc.SendTo(outBuffer, remoteEP);
+
+            clientSoc.Shutdown(SocketShutdown.Both);
+            clientSoc.Close();
+
+            Application.Quit();
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+
+            position = new float[] {myCube.position.x, myCube.position.y, myCube.position.z};
+
+            // Interval set at 25ms & check for movement
+            if ((elapsedTime > 0.025f) && (position != prevPosition))
+            {
+                Buffer.BlockCopy(position, 0, outBuffer, 0, outBuffer.Length);
+                clientSoc.SendTo(outBuffer, remoteEP);
+
+                prevPosition = position;
+                elapsedTime = 0f;
+            }
+        }
+    }
+}
---
+++ Client/Assets/Scripts/Cube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Cube : MonoBehaviour
+{
+    public Vector3 jump = new Vector3(0f, 1f, 0f);
+    public float jumpForce = 2f;
+
+    public bool isGrounded;
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, 1f, 0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        transform.Translate(
+            Input.GetAxis("Horizontal") * Time.deltaTime * 2f, 
+            0f, 
+            Input.GetAxis("Vertical") * Time.deltaTime * 2f
+        );   
+    }
+}
